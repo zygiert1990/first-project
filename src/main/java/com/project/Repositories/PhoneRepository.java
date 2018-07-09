2c9945cc9c056f8475c3b9b38506f1be0ca823c5
zygiert1990 <michal.zyga@pollub.edu.pl>
2018-07-09 18:48:49 +0200

    master commit

--- src/main/java/com/project/Repositories/PhoneRepository.java
+++ src/main/java/com/project/Repositories/PhoneRepository.java
@@ -4,6 +4,8 @@ import com.project.POJOClasses.Phone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PhoneRepository extends JpaRepository<Phone, Long> {
 
@@ -11,4 +13,6 @@ public interface PhoneRepository extends JpaRepository<Phone, Long> {
 
     Phone findById(long id);
 
+    @Override
+    List<Phone> findAll();
 }
