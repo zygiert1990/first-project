0abc193c69a3938371845d4666305324f75d8455
zygiert1990 <michal.zyga@pollub.edu.pl>
2018-08-20 19:26:13 +0200

    commit

--- src/test/java/com/project/Controllers/AddressControllerTest.java
+++ src/test/java/com/project/Controllers/AddressControllerTest.java
@@ -12,14 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import javax.servlet.ServletContext;
 
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
