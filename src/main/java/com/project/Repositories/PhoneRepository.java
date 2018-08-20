f4d8cabe3d82d5b37d4ef5341ce7611195f7eb61
zygiert1990 <michal.zyga@pollub.edu.pl>
2018-08-20 19:58:46 +0200

    second commit

--- src/main/java/com/project/Repositories/PhoneRepository.java
+++ src/main/java/com/project/Repositories/PhoneRepository.java
@@ -1,6 +1,5 @@
 package com.project.Repositories;
 
-import com.project.Model.PhoneType;
 import com.project.POJOClasses.Phone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
